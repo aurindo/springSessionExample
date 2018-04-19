318c55b6efd2ccfa841c04ff3b8362b9fe7dbdef
Aurindo Jorge <aurindo@gmail.com>
2018-04-18 22:23:49 -0300

    First commit

---
+++ src/main/java/br/com/aurindo/springsession/springSessionExample/SpringSessionExampleApplication.java
@@ -0,0 +1,12 @@
+package br.com.aurindo.springsession.springSessionExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionExampleApplication.class, args);
+	}
+}
